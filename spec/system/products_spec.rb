3b20b94c30dad7ddb158a3d50720e7ee9b17f9b0
m <93705485+makoto860@users.noreply.github.com>
2025-12-05 11:31:27 +0900

    Refactor product spec to improve taxon structure

--- spec/system/products_spec.rb
+++ spec/system/products_spec.rb
@@ -1,46 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe "product/show/:id", type: :system do
-  let(:root) { create(:taxon, parent: nil) }
-  let(:other_taxon) { create(:taxon, parent: root) }
-  let(:taxon) { create(:taxon, name: 'taxon', parent: other_taxon) }
-  let!(:product) { create(:product, taxons: [taxon]) }
-  let(:image) { create(:image) }
+  let!(:root_taxon) { create(:taxon, name: "Categories") }
+  let(:parent_taxon) { create(:taxon, parent: root_taxon) }
+  let(:child_taxon) { create(:taxon, name: 'child', parent: parent_taxon) }
+  let!(:image) { create(:image) }
+  # 1つのブロックでproductの生成と画像紐付けを同時に完結
+  let!(:product) do
+    create(:product, taxons: [child_taxon]).tap do |product|
+      product.images << image
+    end
+  end
+  # 4件のrelated_product を生成。順番に名前を"Related Product 1"～"Related Product 4"に設定。それぞれに画像を紐付け。let!で生成しているのでテスト開始前にすべてDBに存在する処理
+  let!(:related_products) do
+    create_list(:product, 4, taxons: [child_taxon]) do |related_product, i|
+      related_product.update!(name: "Related Product #{i + 1}")
+      create(:image, viewable: related_product.master)
+    end
+  end
 
   before do
-    product.images << image
     visit product_path(product.id)
   end
 
-  describe "products/show Crass Container ProductsContainer Test" do
-    it "Crass ProductDetail__name, product.nameが表示されること" do
+  describe "class Container ProductsContainer Test" do
+    it "viewの一緒に見られている商品の場所で関連商品が表示されている" do
+      within('.RelatedProducts') do
+        related_products.each do |related_product|
+          expect(page).to have_content(related_product.name)
+        end
+      end
+    end
+
+    it "class ProductDetail__name, product.nameが表示されること" do
       within('.ProductDetail__name') do
         expect(page).to have_content(product.name)
       end
     end
   end
 
-  describe "Breadcrumb Test" do
-    it "Breadcrumb, taxon.name click, taxon.name一覧へ遷移すること" do
+  describe "shared/_breadcrumb.html.erb Test" do
+    it "class Breadcrumbでchild_taxon.nameをクリックすると、そのカテゴリの商品一覧ページへ遷移すること" do
       within('.Breadcrumb') do
-        click_on taxon.name
-        expect(current_path).to eq category_path(taxon.id)
+        click_on child_taxon.name
+        expect(current_path).to eq category_path(child_taxon.id)
       end
     end
 
-    it "Breadcrumb, other_taxon linkが表示されること" do
+    it "class Breadcrumbでparent_taxon.nameリンクが表示されること" do
       within('.Breadcrumb') do
-        expect(page).to have_link(other_taxon.name)
+        expect(page).to have_link(parent_taxon.name)
       end
     end
 
-    it "Breadcrumb, taxon.name linkが表示されること" do
+    it "class Breadcrumbでchild_taxon.nameリンクが表示されること" do
       within('.Breadcrumb') do
-        expect(page).to have_link(taxon.name)
+        expect(page).to have_link(child_taxon.name)
       end
     end
 
-    it "Breadcrumb, product.nameが表示されること" do
+    it "class Breadcrumbでproduct.nameが表示されること" do
       within('.Breadcrumb') do
         expect(page).to have_content(product.name)
       end
