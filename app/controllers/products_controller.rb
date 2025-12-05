b1216ddb611539a9716255c751438273df8adc48
m <93705485+makoto860@users.noreply.github.com>
2025-12-05 11:26:00 +0900

    Optimize product retrieval with eager loading

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,11 @@
 class ProductsController < ApplicationController
+  RELATED_PRODUCTS_LIMIT = 4
+
   def show
-    # 1つだけ存在する画像を取得する
-    @product = Spree::Product.joins(master: :images).find(params[:id])
+    @product = Spree::Product.includes(master: [:images]).find(params[:id])
+    @related_products = @product.related_products.
+      includes(master: [:images, :prices]).
+      limit(RELATED_PRODUCTS_LIMIT)
     @taxon = @product.taxons.first
     @categories = @taxon.self_and_ancestors.where.not(parent_id: nil)
   end
