7c62616b958b8def03ea46af97892b12288016b7
m <93705485+makoto860@users.noreply.github.com>
2025-12-05 11:30:01 +0900

    Update products_spec.rb

--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -1,16 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe "Products Page", type: :request do
-  describe "GET /products/:show" do
-    let(:taxon) { create(:taxon) }
-    let!(:product) { create(:product, taxons: [taxon]) }
-
-    before do
-      get spree.product_path(product.id)
+  describe "GET /products/:id" do
+    let!(:root_taxon) { create(:taxon, name: 'Categories') }
+    let(:taxon) { create(:taxon, parent: root_taxon) }
+    let(:image) { create(:image) }
+    let(:product) do
+      create(:product, taxons: [taxon]).tap do |product|
+        product.images << image
+      end
+    end
+    let!(:related_products) do
+      create_list(:product, 5, taxons: [taxon]) do |related_product|
+        create(:image, viewable: related_product.master)
+      end
     end
 
-    it '商品名が含まれること' do
-      expect(response.body).to include product.name
+    before do
+      get product_path(product.id)
+    end
+
+    it '関連商品の名前と価格が含まれること' do
+      related_products[0..3].each do |related_product|
+        expect(response.body).to include(related_product.name)
+        expect(response.body).to include(related_product.display_price.to_s)
+      end
+    end
+
+    it '関連商品が5件目は含まれないこと' do
+      expect(response.body).not_to include(related_products[4].name)
+    end
+
+    it "商品名が含まれること" do
+      expect(response.body).to include(product.name)
     end
 
     it '商品詳細ページが200で返ること' do
@@ -24,9 +46,9 @@ RSpec.describe "Products Page", type: :request do
 
   describe "Breadcrumb Test" do
     let(:root_category) { create(:taxon, name: 'Categories', parent: nil) }
-    let(:different_category) { create(:taxon, name: 'different', parent: root_category) }
+    let(:parent_category) { create(:taxon, name: 'parent_category', parent: root_category) }
     let(:child_category) { create(:taxon) }
-    let!(:product) { create(:product, taxons: [different_category]) }
+    let!(:product) { create(:product, taxons: [parent_category]) }
 
     before do
       get spree.product_path(product.id)
@@ -40,8 +62,8 @@ RSpec.describe "Products Page", type: :request do
       expect(response.body).not_to include child_category.name
     end
 
-    it "Breadcrumbにdifferent_category.nameが含まれること" do
-      expect(response.body).to include different_category.name
+    it "Breadcrumbにparent_category.nameが含まれること" do
+      expect(response.body).to include parent_category.name
     end
   end
 end
